afe810d08a19242c2fddb4f32f5b24b220b87ce3
Даниил Джонсон <daniildzonson04@gmail.com>
2021-09-22 14:08:22 +0300

    Доработки

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 # coding=utf-8
+from discord import client
+from discord.flags import Intents
+from discord.message import MessageReference
 from openpyxl import load_workbook, Workbook
 wb = load_workbook('Analusis.xlsx')
 ws = wb.active
 import discord
 from discord.ext import commands, tasks
-
  
-bot = commands.Bot("!")
-target_channel_id = 886915166120345670
-target_channel_id1 = 652634813576118335
+bot = commands.Bot(command_prefix='!', self_bot=True)
+target_channel_id = 652634813576118335
+
 
 @bot.event
 async def on_message(message):
-    if message.channel.id == target_channel_id1:
-        print('New message!')
-        ifContainsAndStore(message)
+    if message.channel.id == target_channel_id:
+        async for message in message.channel.history(limit=1):
+            print("Новое объявление!")
+            ifContainsAndStore(message)
+        
 
 def ifContainsAndStore(string):
     if string.content.find("родам") >= 0:
@@ -34,7 +38,7 @@ def ifContainsAndStore(string):
             storeData(string.content, 9, "Продаётся KIA Stinger", "H")
         elif string.content.find("R34") >= 0 or string.content.find("скайлик") >= 0 and string.content.find("34") >= 0:
             storeData(string.content, 10, "Продаётся Skyline R34", "I")
-        elif string.content.find("e70") >= 0 or string.content.find("x5") >= 0 or string.content.find("X5") >= 0:
+        elif string.content.find("X5 E70") >= 0 or string.content.find("x5 e70") >= 0 or string.content.find("X5 e70") >= 0:
             storeData(string.content, 11, "Продаётся BMW x5 e70", "J")
         elif string.content.find("Alfa") >= 0 or string.content.find("альфу") >= 0:
             storeData(string.content, 12, "Продаётся Alfa Romeo", "K")
@@ -50,7 +54,7 @@ def ifContainsAndStore(string):
             storeData(string.content, 17, "Продаётся Lexus LX570", "P")
         elif string.content.find("Model S") >= 0 :
             storeData(string.content, 18, "Продаётся Tesla Model S", "Q")
-        elif string.content.find("G63") >= 0 :
+        elif string.content.find("G63") >= 0 or string.content.find("гелик") >= 0 or string.content.find("Гелик") >= 0:
             storeData(string.content, 19, "Продаётся Mercedes G63", "R")
         elif string.content.find("RS7") >= 0 :
             storeData(string.content, 20, "Продаётся Audi RS7", "S")
@@ -60,11 +64,14 @@ def ifContainsAndStore(string):
 def storeData(string, int, comment, letter):
     if convertToMoney(string) != "Цена договорная!":
         converted = convertToMoney(string)
-        x = ws[f'A{int}'].value
-        ws[f'{letter}{x}'].value = converted
-        ws[f'A{int}'].value = (x + 1)
-        wb.save('Analusis.xlsx')
-        print(comment, "за", converted)
+        if converted != 0:
+            x = ws[f'A{int}'].value
+            ws[f'{letter}{x}'].value = converted
+            ws[f'A{int}'].value = (x + 1)
+            wb.save('Analusis.xlsx')
+            print(comment, "за", converted)
+        else:
+            print("Ошибка в получении цены!")
     else:
         print("Цена договорная и она не записана в таблицу!")  
 
@@ -80,6 +87,8 @@ def convertToMoney(string):
     for str in string.split():
         if str.find("кк") >= 0 or str.find(",") >= 0 or str.find(".") >= 0 or str.find("к") >= 0 or startWithDigit(str) == 1 or str.find("kk") >= 0 or str.find("k") >= 0:
             if startWithDigit(str) == 1:
+                if str.find('.') == 1 and str.find('.', 2) == 3 and str.find('.', 4) == 5:
+                    return
                 if str.find('.') == 1 or str.find(',') == 1 and str.find('кк') >= 0 or str.find('kk') >= 0: # проверка формата
                     for str1 in str:
                         if block <= 2:
--- toMoney.py
+++ toMoney.py
@@ -1,4 +1,34 @@
-elif str.find('.') == 1 and str.find('.', 2) == 5: # проверка формата
+def convertToMoney(string):
+    if string.find('догов') >= 0 or string.find('Догов') >= 0 or string.find('лс') >= 0:
+        return "Цена договорная!"
+    bool = 0
+    millions = 0
+    thousands = 0
+    hundreds = 0
+    block = 0
+    count = 0
+    for str in string.split():
+        if str.find("кк") >= 0 or str.find(",") >= 0 or str.find(".") >= 0 or str.find("к") >= 0 or startWithDigit(str) == 1 or str.find("kk") >= 0 or str.find("k") >= 0:
+            if startWithDigit(str) == 1:
+                if str.find('.') == 1 or str.find(',') == 1 and str.find('кк') >= 0 or str.find('kk') >= 0: # проверка формата
+                    for str1 in str:
+                        if block <= 2:
+                            block = block + 1
+                            if str1.isdigit() == 1 and bool == 0:
+                                millions = int(str1)
+                            if str1 == "." or str1 == ",":
+                                bool = 1
+                            if str1.isdigit() == 1 and bool == 1:
+                                thousands = int(str1)
+                if str.find('.') == 3 and str.find('.') != 1 or str.find('к') == 3 and str.find('.') != 1 or str.find('k') == 3 and str.find('.') != 1: # проверка формата
+                    count = 0
+                    for str1 in str:
+                        if count == 0:
+                            thousands = int(str1)
+                        elif count == 2:
+                            hundreds = int(str1)
+                        count = count + 1
+                elif str.find('.') == 1 and str.find('.', 2) == 5: # проверка формата
                         for str1 in str:
                             if count <= 3:
                                 if str1.isdigit() and count == 0:
@@ -10,3 +40,18 @@ elif str.find('.') == 1 and str.find('.', 2) == 5: # проверка форма
                                 elif str1.isdigit() and count == 2:
                                     hundreds = int(str1)
                                     count = count + 1
+                    
+                                         
+    millions = millions * 1000000
+    thousands = thousands * 100000
+    hundreds = hundreds * 10000
+    millions = millions + thousands + hundreds
+    return millions
+
+def startWithDigit(str):
+    if str.startswith('1') or str.startswith('2') or str.startswith('3') or str.startswith('4') or str.startswith('5') or str.startswith('6') or str.startswith('7') or str.startswith('8') or str.startswith('9'):
+        return 1
+    else:
+        return 0
+
+convertToMoney("5.1")
